64d0c01d3eaaed7fe17e2dddc4472da1008c4898
bbrisch <benjaminbritoschiele@gmail.com>
2023-03-27 12:18:53 -0300

    debugging

--- func.py
+++ func.py
@@ -4,6 +4,7 @@
 
 import numpy as np
 import plotly.express as px
+import plotly.graph_objects as go
 
 # ============================ Funciones del enunciado ============================================
 
@@ -63,22 +64,21 @@ def de_a(conjunto1: list, conjunto2: list) -> list:
 
 
 # Parte d): Se genera una función que permite visualizar el grado de pertenencia a cada regla dadas dos entradas
-def base_reglas(input1: float, input2: float, conjuntos: list, reglas: list):
+def base_reglas(input1: float, input2: float, reglas: list):
     """
     Función que computa manualmente el grado de activación de cada regla y sus entradas.
 
     - input1/2: Numeros de entrada a la base de reglas
-    - conjuntos: lista con los conjuntos a operar
     - reglas: lista con diccionarios con reglas a operar
 
     Estructura del diccionairio:
         entrada1:
-            - numero del conjunto
-            - numero del conjunto
+            - conjunto
+            - conjunto
 
         entrada2:
-            - numero del conjunto
-            - numero del conjunto
+            - conjunto
+            - conjunto
 
         salida: conjunto de la salida
     """
@@ -87,21 +87,73 @@ def base_reglas(input1: float, input2: float, conjuntos: list, reglas: list):
     E1 = []
     E2 = []
     Salida = []
+    S_conj = []
 
     # Iteramos por las reglas
     for regla in reglas:
-
+        # print(regla)
         # Computamos las pertenencias
         e1 = interfaz_difusion(input1, de_a(*regla["entrada1"]))
         e2 = interfaz_difusion(input2, de_a(*regla["entrada2"]))
-        s = interfaz_difusion(min(e1, e2), regla["salida"])
+        # print(e1, e2, min(e1, e2), "\n")
+        # s = interfaz_difusion(min(e1, e2), regla["salida"])
+
+        # Truncamos las reglas para obtener el conjunto de salida
+        # S_conj.append([regla["salida"][0], s, s, regla["salida"][-1]])
 
         # Guardamos los resultados
         E1.append(e1)
         E2.append(e2)
-        Salida.append(s)
+        Salida.append(min(e1, e2))
 
-    return E1, E2, Salida
+    return (
+        E1,
+        E2,
+        Salida,
+    )  # S_conj
+
+
+# Parte e): Implemente una máquina de inferencia que ocupe una base de conocimientos con reglas difusas
+def maquina_de_inferencia(input1: float, input2: float, reglas: list, n: int = 41):
+
+    # Aplicamos las reglas y obtenemos su activación
+    _, _, activaciones = base_reglas(input1, input2, reglas)
+
+    # Sampleamos las activaciones
+    cdm_ = 0
+    acum = 0
+
+    # Iteramos cada punto
+    for i in np.linspace(-1, 1, n):
+        aux = []
+        # Verificamos la activación el el punto de cada regla
+        for regla, activacion in zip(reglas, activaciones):
+            # print(regla, activacion,)
+
+            pertenencia = interfaz_difusion(i, regla["salida"])
+            # print(
+            #     "i",
+            #     i,
+            #     "\tregla",
+            #     regla["salida"],
+            #     "\tactivación",
+            #     activacion,
+            #     "\tpertenencia",
+            #     pertenencia,
+            #     "\tactivación de regla",
+            #     pertenencia if pertenencia < activacion else activacion,
+            # )
+            aux.append(pertenencia if pertenencia < activacion else activacion)
+        # print("\n")
+
+        cdm_ += i * max(aux)  # Consideramos solo la máxima activación
+        acum += max(aux)
+        # print(max(aux), i * max(aux))
+        # print(acum, cdm_)
+        # break
+    print(cdm_, acum)
+    # Retornamos el centro de masa
+    return cdm_ / acum if acum != 0 else 0
 
 
 # ============================ Funciones extra ============================================
@@ -114,5 +166,7 @@ def graficar_conjunto_difuso(mu, conjunto, n=43):
     eje = np.linspace(-1, 1, n)
     pertenencia = [mu(x, conjunto) for x in eje]
 
-    return px.scatter(x=eje, y=pertenencia)
+    # return px.scatter(x=eje, y=pertenencia)
     # return eje, pertenencia
+
+    return go.Scatter(x=eje, y=pertenencia)
